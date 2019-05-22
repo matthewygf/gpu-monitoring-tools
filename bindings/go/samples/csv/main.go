f93838077a6890fbe5727bc6b28c02ff92590e81
Matthew Yeung <yyygggfff@hotmail.com>
2019-05-22 11:06:42 +0100

    testing nil declartion and defer

--- bindings/go/samples/csv/main.go
+++ bindings/go/samples/csv/main.go
@@ -2,26 +2,54 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
+	"fmt"
 	"log"
 	"os"
 )
 
-var data = [][]string{{"line1", "hello readers of"}, {"line2", "golang"}}
+var usecsv = flag.Bool("csv", false, "csv format")
+var path = flag.String("logpath", "processinfo.csv", "file path for logging")
 
 func main() {
-	fileHandle, err := os.Create("test.csv")
-	checkError("File not created", err)
-	// until the end of main()
-	defer fileHandle.Close()
+	flag.Parse()
+	fmt.Printf("%v\n", *path)
+	fmt.Printf("%t\n", *usecsv)
 
-	writer := csv.NewWriter(fileHandle)
-	// until the end of main()
-	defer writer.Flush()
-
-	for _, value := range data {
-		err := writer.Write(value)
-		checkError("cannot write", err)
+	var fileHandle *os.File
+	var err error
+	var writer *csv.Writer
+	if *usecsv {
+		fileHandle, err = os.Create("test.csv")
+		checkError("couldn't create", err)
+		defer func() {
+			fmt.Printf("gonna close \n")
+			fileHandle.Close()
+		}()
 	}
+
+	if fileHandle != nil {
+		writer = csv.NewWriter(fileHandle)
+		header := []string{"1", "2", "3", "4", "5"}
+		err := writer.Write(header)
+		checkError("couldn't write", err)
+		writer.Flush()
+	}
+
+	fmt.Printf("i did some stuff \n")
+	// fileHandle, err := os.Create("test.csv")
+	// checkError("File not created", err)
+	// // until the end of main()
+	// defer fileHandle.Close()
+
+	// writer := csv.NewWriter(fileHandle)
+	// // until the end of main()
+	// defer writer.Flush()
+
+	// for _, value := range data {
+	// 	err := writer.Write(value)
+	// 	checkError("cannot write", err)
+	// }
 }
 
 func checkError(message string, err error) {
